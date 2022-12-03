eebd0296ba9197a3196b7ac0e96e1b3e961ae9e6
BuildTools <unconfigured@null.spigotmc.org>
2022-12-03 11:44:33 +0100

    Initial commit

---
+++ Python files/!Codici programmi/update git.py
@@ -0,0 +1,12 @@
+# Questo software aggiorna la repository di GitHub automaticamente muovendo il mouse al posto tuo.
+# By iThinkAle
+
+import pyautogui as pag
+
+print("Aggiornando repository di GitHub...")
+pag.moveTo(1744, 85, 0.2)
+pag.click()
+pag.moveTo(183, 746, 0.2)
+pag.click()
+pag.moveTo(1201, 792, 0.2)
+
