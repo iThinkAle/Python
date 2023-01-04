897395d9cecbdca0bef8510b06faff771cdb2b34
BuildTools <unconfigured@null.spigotmc.org>
2023-01-05 00:44:23 +0100

    Commit

--- Python files/!Codici programmi/update git.py
+++ Python files/!Codici programmi/update git.py
@@ -3,7 +3,7 @@
 # DISCLAIMER: CODESTO PROGRAMMA FUNZIONA SOLO IN PYCHARM CON FINESTRA A SCHERMO INTERO. IN ALTRI PROGRAMMI O NEL CASO
 # PYCHARM NON DOVESSE ESSERE SCHERMO INTERO IL PROGRAMMA NON FUNZIONERA'.
 
-import time
+
 import pyautogui as pag
 import keyboard as kb
 
@@ -11,9 +11,6 @@ import keyboard as kb
 print("Aggiornando repository di GitHub...")
 pag.moveTo(1744, 85, 0.2)
 pag.click()
-pag.alert("Ora scrivere il messaggio e selezionare i file da aggiornare nella repository. Appena finito, premere F1")
-time.sleep(2)
-kb.is_pressed("enter")
 pag.moveTo(169, 767, 0.2)
 pag.click()
 pag.moveTo(1201, 792, 0.2)
