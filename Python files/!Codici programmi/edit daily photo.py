6858922c957af41d4cc5429f5c65d986d5ac1939
BuildTools <unconfigured@null.spigotmc.org>
2023-01-31 19:29:31 +0100

    Commit

--- Python files/!Codici programmi/edit daily photo.py
+++ Python files/!Codici programmi/edit daily photo.py
@@ -2,4 +2,4 @@ import os
 
 os.startfile("G:\Il mio Drive\!Daily Photos\Sample Photo.psd")
 os.startfile("G:\Il mio Drive\!Daily Photos\Originali")
-os.startfile("G:\Il mio Drive\!Daily Photos\Finali")
+os.startfile("G:\Il mio Drive\!Daily Photo")
