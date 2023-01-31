2726400a1e33d59df9f15509a484794afc6cc1b4
BuildTools <unconfigured@null.spigotmc.org>
2023-01-31 20:20:26 +0100

    Try to find a better module

---
+++ Python files/!Codici programmi/currency converter.py
@@ -0,0 +1,23 @@
+from currency_converter import CurrencyConverter
+import datetime
+c = CurrencyConverter()
+
+
+now = datetime.datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+
+def convert():
+    try:
+        newcurrency = c.convert(quantity, currency1, currency2)
+        print(f"{quantity} in {currency2} (alle {dt_string}) è: {newcurrency}")
+
+    except ValueError:
+        print("Errore! Valuta non supportata!")
+
+
+print("Benvenuto nel convertitore di valuta.")
+currency1 = input("Inserisci la valuta da convertire (codice (es: USD, EUR)): ")
+currency2 = input("Inserisci la valuta a cui vuoi che la tua cifra sia convertita (codice): ")
+quantity = float(input("Inserisci la cifra di denaro da convertire: "))
+convert()
