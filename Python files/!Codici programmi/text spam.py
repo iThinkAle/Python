95e85fef79959f06dce67173e693f955d420a3cb
BuildTools <unconfigured@null.spigotmc.org>
2023-02-04 11:14:43 +0100

    Added try except

--- Python files/!Codici programmi/text spam.py
+++ Python files/!Codici programmi/text spam.py
@@ -1,31 +1,31 @@
 # Questo programma scrive ripetutamente una frase prefissata per un numero di volte settato
 # By iThinkAle
 
-
 import pyautogui as pag
 import sys
 
 
 def main():
     global text, number, time
-    text = input(str("Digita il testo da spammare: "))
-    number = int(input("Digita il numero di volte che vuoi che il testo venga scritto: "))
-    time = float(input("Digita ogni quanto tempo (in secondi) vuoi che il testo venga scritto: "))
-    program()
+    try:
+        text = input(str("Digita il testo da spammare: "))
+        number = int(input("Digita il numero di volte che vuoi che il testo venga scritto: "))
+        time = float(input("Digita ogni quanto tempo (in secondi) vuoi che il testo venga scritto: "))
+        program()
+    except ValueError:
+        print("Errore! Metti gli input al posto giusto! Riprova!")
+        main()
 
 
 def program():
-    try:
-        count = 0
-        while count < int(number):
-            pag.typewrite(text)
-            pag.press("enter")
-            pag.sleep(int(time))
-            count += 1
-        else:
-            sys.exit()
-    except KeyboardInterrupt:
-        print("A presto!")
+    count = 0
+    while count < int(number):
+        pag.typewrite(text)
+        pag.press("enter")
+        pag.sleep(int(time))
+        count += 1
+    else:
+        sys.exit()
 
 
 if __name__ == "__main__":
