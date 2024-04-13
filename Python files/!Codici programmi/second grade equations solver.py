2b5afd2ac4c87a4c65a96ddaac5a716bd5a4171f
Alessandro Ricordy <alessandrorico@outlook.it>
2024-04-13 20:44:34 +0200

    Commit

--- Python files/!Codici programmi/second grade equations solver.py
+++ Python files/!Codici programmi/second grade equations solver.py
@@ -12,14 +12,14 @@ b = binput
 c = cinput
 
 try:
-    deltacalc = math.sqrt((b**2)-(4*a*c))
+    delta = math.sqrt((b**2)-(4*a*c))
 except ValueError:
     print("No solution exists")
 else:
     pass
 
-x1 = (-b+deltacalc)/(2*a)
-x2 = (-b-deltacalc)/(2*a)
+x1 = (-b+delta)/(2*a)
+x2 = (-b-delta)/(2*a)
 
 print(f"x1 = {x1}, x2 = {x2}")
-print(f"delta = {deltacalc}")
+print(f"delta = {delta}")
