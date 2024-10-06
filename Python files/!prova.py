049ac44a8b12ed0cf9a4193bc47d9c218e45bbee
Alessandro Ricordy <alessandrorico@outlook.it>
2024-10-06 17:41:49 +0200

    Commit

--- Python files/!prova.py
+++ Python files/!prova.py
@@ -1,2 +1,9 @@
-for x in "banana":
-  print(x)
+maschi = 0
+for i in range(21):
+    alunno = input("Digitare il sesso dell'alunno: ")
+    if alunno == "maschio":
+        maschi = maschi + 1
+        continue
+    else:
+        continue
+print(maschi)
